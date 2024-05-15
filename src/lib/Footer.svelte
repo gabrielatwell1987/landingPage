ecb487d25edb5a8996bf7f4a3936ac12ecc1039d
gabrielatwell1987 <gabrielatwell@proton.me>
2024-05-15 12:06:07 -0700

    editted hero and footer components

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,7 +2,7 @@
 
 <footer class="bg-gray-100">
 	<div class="container mx-auto px-6 pb-6 pt-10">
-		<div class="ml-52 flex flex-wrap">
+		<div class="flex flex-wrap">
 			<div class="w-full text-center md:w-1/4 md:text-left">
 				<h5 class="mb-6 font-bold uppercase">Links</h5>
 				<ul class="mb-4">
--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -2,12 +2,10 @@
 
 <div class="py-20" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)">
 	<div class="container mx-auto px-6">
-		<h2 class="mb-2 ml-24 text-4xl font-bold text-white">A distinct landing page!</h2>
-		<h3 class="mb-8 ml-24 text-2xl text-gray-200">Made with sveltekit and tailwind..</h3>
+		<h2 class="mb-2 text-4xl font-bold text-white">A distinct landing page!</h2>
+		<h3 class="mb-8 text-2xl text-gray-200">Made with sveltekit and tailwind..</h3>
 
-		<button
-			class="ml-24 rounded-full bg-white px-8 py-4 font-bold uppercase tracking-wider shadow-lg"
-		>
+		<button class=" rounded-full bg-white px-8 py-4 font-bold uppercase tracking-wider shadow-lg">
 			Learn More
 		</button>
 	</div>
